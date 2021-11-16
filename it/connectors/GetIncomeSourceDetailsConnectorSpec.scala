0244f1203fee50777918df898408912ed74bea7a
Robert Webb <28566263+robert-webb-hmrc@users.noreply.github.com>
2021-11-16 10:59:50 +0000

    SASS-1611 : Removed Injection from Unit Tests

--- it/connectors/GetIncomeSourceDetailsConnectorSpec.scala
+++ it/connectors/GetIncomeSourceDetailsConnectorSpec.scala
@@ -17,7 +17,7 @@
 package connectors
 
 import com.github.tomakehurst.wiremock.http.HttpHeader
-import config.AppConfig
+import config.{AppConfig, BackendAppConfig}
 import helpers.WiremockSpec
 import models.{DesErrorBodyModel, DesErrorModel, InterestDetailsModel}
 import org.scalatestplus.play.PlaySpec
@@ -39,7 +39,7 @@ class GetIncomeSourceDetailsConnectorSpec extends PlaySpec with WiremockSpec{
 
   lazy val httpClient: HttpClient = app.injector.instanceOf[HttpClient]
 
-  def appConfig(desHost: String): AppConfig = new AppConfig(app.injector.instanceOf[Configuration], app.injector.instanceOf[ServicesConfig]) {
+  def appConfig(desHost: String): AppConfig = new BackendAppConfig(app.injector.instanceOf[Configuration], app.injector.instanceOf[ServicesConfig]) {
     override val desBaseUrl: String = s"http://$desHost:$wireMockPort"
   }
 
