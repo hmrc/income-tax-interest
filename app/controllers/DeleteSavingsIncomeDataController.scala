f2f327edc20e2898a5a8a4c036d76acdaf42113c
Kannan Ramakrishnan <12591116+kannanramakrishnan@users.noreply.github.com>
2022-10-10 12:59:13 +0100

    SASS-3362 Delete Savings Income Submission added

---
+++ app/controllers/DeleteSavingsIncomeDataController.scala
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import controllers.predicates.AuthorisedAction
+import play.api.mvc.{Action, AnyContent, ControllerComponents}
+import services.DeleteSavingsIncomeDataService
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+
+import javax.inject.Inject
+import scala.concurrent.ExecutionContext
+
+class DeleteSavingsIncomeDataController @Inject()(deleteSavingsIncomeDataService: DeleteSavingsIncomeDataService,
+                                                  cc: ControllerComponents,
+                                                  authorisedAction: AuthorisedAction)
+                                                 (implicit ec: ExecutionContext) extends BackendController(cc) {
+
+  def deleteSavingsIncomeData(nino: String, taxYear: Int): Action[AnyContent] = authorisedAction.async {
+    implicit user =>
+      deleteSavingsIncomeDataService.deleteSavingsIncomeData(nino, taxYear).map {
+        case Right(true) => NoContent
+        case Left(errorModel) => Status(errorModel.status)(errorModel.toJson)
+      }
+  }
+}
---
+++ test/services/DeleteSavingsIncomeDataSpec.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package services
+
+import connectors.DeleteSavingsIncomeDataConnector
+import connectors.httpParsers.DeleteSavingsIncomeDataParser.DeleteSavingsIncomeDataResponse
+import testUtils.TestSuite
+import uk.gov.hmrc.http.HeaderCarrier
+
+import scala.concurrent.Future
+class DeleteSavingsIncomeDataSpec extends TestSuite{
+  val connector: DeleteSavingsIncomeDataConnector = mock[DeleteSavingsIncomeDataConnector]
+  val service: DeleteSavingsIncomeDataService = new DeleteSavingsIncomeDataService(connector)
+
+  "DeleteSavingsIncomeData" should {
+
+    "return the connector response" in {
+
+      val expectedResult: DeleteSavingsIncomeDataResponse = Right(true)
+
+      (connector.deleteSavingsIncomeData(_: String, _: Int)(_: HeaderCarrier))
+        .expects("12345678", 1234, *)
+        .returning(Future.successful(expectedResult))
+
+      val result = await(service.deleteSavingsIncomeData("12345678", 1234))
+
+      result mustBe expectedResult
+
+    }
+  }
+}
