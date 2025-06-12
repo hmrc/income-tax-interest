bceac84ad04adf892b4bbe5af0ecc75e5a736fd9
Amaal Ali <9220299+amaalali@users.noreply.github.com>
2025-06-12 16:20:30 +0100

    Compose reads for error model for more ergonomic parsing

--- app/models/ErrorBody.scala
+++ app/models/ErrorBody.scala
@@ -16,30 +16,34 @@
 
 package models
 
-import play.api.libs.json.{JsValue, Json, OFormat}
+import play.api.libs.json.{JsValue, Json, OFormat, Reads}
 
 sealed trait ErrorBody
-
-case class ErrorModel(status: Int, body: ErrorBody){
-  def toJson: JsValue ={
-    body match {
-      case error: ErrorBodyModel => Json.toJson(error)
-      case errors: ErrorsBodyModel => Json.toJson(errors)
-    }
-  }
+object ErrorBody {
+  implicit val reads: Reads[ErrorBody] =
+    ErrorBodyModel.formats.widen[ErrorBody] orElse ErrorsBodyModel.formats.widen[ErrorBody]
 }
 
 /** Single Error **/
 case class ErrorBodyModel(code: String, reason: String) extends ErrorBody
-
 object ErrorBodyModel {
   implicit val formats: OFormat[ErrorBodyModel] = Json.format[ErrorBodyModel]
   val parsingError: ErrorBodyModel = ErrorBodyModel("PARSING_ERROR", "Error parsing response from API")
 }
 
+object EmptyErrorBody  extends ErrorBody
+
 /** Multiple Errors **/
 case class ErrorsBodyModel(failures: Seq[ErrorBodyModel]) extends ErrorBody
-
 object ErrorsBodyModel {
   implicit val formats: OFormat[ErrorsBodyModel] = Json.format[ErrorsBodyModel]
+}
+
+case class ErrorModel(status: Int, body: ErrorBody) {
+  def toJson: JsValue =
+    body match {
+      case error @ ErrorBodyModel(_, _) => Json.toJson(error)
+      case errors @ ErrorsBodyModel(_) => Json.toJson(errors)
+      case EmptyErrorBody => Json.obj()
+    }
 }
