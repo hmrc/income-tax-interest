534f27fdf809b08bb44a3a8336bc8e34595fbf1c
Farnoosh Moayeri <91014204+Farnooshmo@users.noreply.github.com>
2026-02-27 11:58:05 +0000

    Upgrade dependencies (#82)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -18,7 +18,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.5.0"
+  private val bootstrapVersion = "10.6.0"
   private val hmrcMongoPlay30Version = "2.12.0"
 
   private val jacksonAndPlayExclusions: Seq[InclusionRule] = Seq(
@@ -32,11 +32,11 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                   %% "bootstrap-backend-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-30"         % hmrcMongoPlay30Version,
-    "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.21.0",
+    "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.21.1",
     "uk.gov.hmrc"                   %% "crypto-json-play-30"        % "8.4.0",
     "org.typelevel"                 %% "cats-core"                  % "2.13.0",
-    "com.beachape"                  %% "enumeratum"                 % "1.9.4",
-    "com.beachape"                  %% "enumeratum-play-json"       % "1.9.4" excludeAll (jacksonAndPlayExclusions *)
+    "com.beachape"                  %% "enumeratum"                 % "1.9.5",
+    "com.beachape"                  %% "enumeratum-play-json"       % "1.9.5" excludeAll (jacksonAndPlayExclusions *)
   )
 
   val test: Seq[ModuleID] = Seq(
